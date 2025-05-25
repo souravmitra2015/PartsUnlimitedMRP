6c1eb1b312f7468f025d124129aac566a02cd437
Sourav Mitra <sourav.mitra2015@outlook.com>
2025-05-25 16:25:16 +0530

    My first change

--- src/Clients/Web/js/date.js
+++ src/Clients/Web/js/date.js
@@ -1,5 +1,6 @@
 /**
  * Version: 1.0 Alpha-1 
+ * This is test
  * Build Date: 13-Nov-2007
  * Copyright (c) 2006-2007, Coolite Inc. (http://www.coolite.com/). All rights reserved.
  * License: Licensed under The MIT License. See license.txt and http://www.datejs.com/license/. 
